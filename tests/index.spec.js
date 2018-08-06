6caaa06247a9d094d7b28b37d2b3639931ba2ef3
Charles Stover <github@charlesstover.com>
2018-08-05 23:04:19 -0500

    jest

---
+++ tests/index.spec.js
@@ -0,0 +1,13 @@
+import Enzyme, { render, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import React from 'react';
+import Quotes from '../index';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('react-quotes', () => {
+
+  it('should render without crashing', () => {
+    shallow(<Quotes />);
+  });
+});
