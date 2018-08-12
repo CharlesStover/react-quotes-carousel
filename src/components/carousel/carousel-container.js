a348e6c1f0e4e136ad0f8441173145464c3227f1
Charles Stover <github@charlesstover.com>
2018-08-12 16:37:16 -0500

    state

--- src/components/carousel/carousel-container.js
+++ src/components/carousel/carousel-container.js
@@ -2,6 +2,11 @@ import React from 'react';
 import View from './carousel-view';
 
 export default class Carousel extends React.PureComponent {
+
+  state = {
+    index: 0
+  };
+
   render() {
     return (
       <View
