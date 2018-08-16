1bc39e2fed2c902abdd111f6eb12028785deff79
Charles Stover <github@charlesstover.com>
2018-08-15 23:34:16 -0500

    travis ci

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,4 +1,4 @@
-import Enzyme, { render, shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
 import Quotes from '../index';
