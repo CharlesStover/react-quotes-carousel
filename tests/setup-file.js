fabc9787542d7b042e5af7d6041b5074ce829aa6
Charles Stover <github@charlesstover.com>
2018-08-15 23:37:17 -0500

    raf polyfill

--- tests/setup-file.js
+++ tests/setup-file.js
@@ -1,6 +1,6 @@
 require('jsdom-global')();
 
-global.cancelAnimationFrame = i => global.window.clearTimeout(i);
-global.requestAnimationFrame = f => global.window.setTimeout(f, 0);
-global.window.cancelAnimationFrame = global.cancelAnimationFrame;
-global.window.requestAnimationFrame = global.requestAnimationFrame;
+global.cancelAnimationFrame = () => {};
+global.requestAnimationFrame = () => 0;
+global.window.cancelAnimationFrame = () => {};
+global.window.requestAnimationFrame = () => 0;
